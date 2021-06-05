f58f98c61b2d7ec30f7e98508ba7d36d4d08d5bd
guus7avo <49452566+guus7avo@users.noreply.github.com>
2021-06-05 15:08:03 -0300

    Delete conecta.php

--- conecta.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$host = "localhost";
-	$user = "root";
-	$pass = "";
-	$banco = "sistemacadastro";
-	$conexao = mysqli_connect($host, $user, $pass, $banco) or die(mysqli_error());
-	mysqli_select_db($conexao, $banco) or die(mysqli_error());
-?>
